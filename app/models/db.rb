12c64711cfb8ea4ad3b1cc684e64e92226ab49c4
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-05-19 16:37:15 +0200

    fixed production db connection

--- app/models/db.rb
+++ app/models/db.rb
@@ -3,7 +3,7 @@
 
 case RACK_ENV
 when 'production'
-  DB = Sequel.postgres('sellastic_prod', :user => 'sellastic', :password => 'sellastic', :host => 'localhost')
+  DB = Sequel.postgres('sellastic_production', :user => 'sellastic', :password => 's3ll4stic', :host => 'localhost')
 when 'development'
   DB = Sequel.postgres('sellastic_development', :user => 'sellastic', :host => 'localhost')
 when 'test'
